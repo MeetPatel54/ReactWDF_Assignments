86be18110513b1331e67f119dcbfbf7ee2578664
Pmu782005 <pmeet782005@gmail.com>
2025-02-11 13:18:11 +0530

    v0.0.1

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import Footer from '../components/Footer.jsx'
+import Greet from '../components/Greet.jsx'
+import Header from '../components/Header.jsx'
+import MainComponent from '../components/MainComponent.jsx'
+
+
+function App() {
+  
+
+  return (
+    <>
+     <Greet />
+     <Header />
+     <MainComponent />
+     <Footer />
+     </>
+  )
+}
+
+export default App
