58eb79cee4cdf726636f56d569661cd031d36be2
Pmu782005 <pmeet782005@gmail.com>
2025-02-11 21:01:46 +0530

    modified

--- Task-3/src/App.jsx
+++ Task-3/src/App.jsx
@@ -1,5 +1,4 @@
 import Footer from '../components/Footer.jsx'
-import Greet from '../components/Greet.jsx'
 import Header from '../components/Header.jsx'
 import MainComponent from '../components/MainComponent.jsx'
 
